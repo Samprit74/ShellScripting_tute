9a5b86b92c621399dcf9f3901f63b7adce024c8c
Samprit Roy <sampritroy99@gmail.com>
2026-02-26 11:48:46 +0530

    Day02: Added variables and user creation scripts

---
+++ day02/hellow2.sh
@@ -0,0 +1,24 @@
+
+#!/bin/bash
+
+#This is script for practice
+
+echo "Hellow user"
+
+echo "Giving first echo commands and if it succeed this line will show up"
+
+echo "Good morning"
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
---
+++ day02/variables.sh
@@ -0,0 +1,21 @@
+
+#!/bin/bash
+
+#This is day 2 of practice
+
+:<<'comment'
+    this is a multi line comment .
+   Anything  written here will not be executed 
+comment
+
+
+name="Samprit Roy"
+
+echo "name is $name, and date is $(date)"
+
+read username
+
+echo "You entered $username"
+
+echo "The charecters in $0 are:  $1 and $2 and $3"
+
